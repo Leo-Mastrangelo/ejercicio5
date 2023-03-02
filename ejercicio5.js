b830dedfc1146e5468ec5de45468dc4c2cb34bf4
Leo-Mastrangelo <109768179+Leo-Mastrangelo@users.noreply.github.com>
2023-03-02 09:56:38 -0300

    Add files via upload

---
+++ ejercicio5.js
@@ -0,0 +1,11 @@
+let alturacm = 172
+let alturam = 1.72
+let pesokg = 76.2
+//altura en metros redondeada hacia arriba
+console.log (alturam.toFixed())
+
+//peso en kilogramos redondeado hacia abajo
+console.log (pesokg.toPrecision(2))
+
+//- Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" 
+//es igual al máximo valor que se puede obtener en Javascript
